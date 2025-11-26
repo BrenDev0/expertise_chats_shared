5b16e54ff5c515e0808602babdb6a0a1af058f25
BrenDev0 <lahey1991@gmail.com>
2025-11-26 17:39:08 -0600

    added  event handlers

--- expertise_chats/broker/__init__.py
+++ expertise_chats/broker/__init__.py
@@ -6,10 +6,13 @@ Message broker package for expertise_chats.
 from expertise_chats.broker.infrastructure.rabbitMq.connection import RabbitMqConnection as BrokerConnection
 from expertise_chats.broker.infrastructure.rabbitMq.consumer import RabbitMqConnection as Consumer
 from expertise_chats.broker.infrastructure.rabbitMq.producer import RabbitMqProducer as Producer
+from expertise_chats.broker.domain.event_handler import EventHandler, AsyncEventHandler
 
 # Re-export the main classes users need
 __all__ = [
    "BrokerConnection",
    "Consumer",
-   "Producer"
+   "Producer",
+   "EventHandler",
+   "AsyncEventHandler"
 ]
