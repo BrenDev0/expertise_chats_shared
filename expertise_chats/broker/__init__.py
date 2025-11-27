2f3fa7f1027afa8e13d3c4d753349db04a89829e
BrenDev0 <lahey1991@gmail.com>
2025-11-27 10:04:19 -0600

    baseevet export

--- expertise_chats/broker/__init__.py
+++ expertise_chats/broker/__init__.py
@@ -7,6 +7,7 @@ from expertise_chats.broker.infrastructure.rabbitMq.connection import RabbitMqCo
 from expertise_chats.broker.infrastructure.rabbitMq.consumer import RabbitMqConsumer as Consumer
 from expertise_chats.broker.infrastructure.rabbitMq.producer import RabbitMqProducer as Producer
 from expertise_chats.broker.domain.event_handler import EventHandler as EventHandlerBase, AsyncEventHandler as AsyncEventHandlerBase
+from expertise_chats.broker.domain.base_event import BaseEvent
 
 # Re-export the main classes users need
 __all__ = [
@@ -14,5 +15,6 @@ __all__ = [
    "Consumer",
    "Producer",
    "EventHandlerBase",
-   "AsyncEventHandlerBase"
+   "AsyncEventHandlerBase",
+   "BaseEvent"
 ]
