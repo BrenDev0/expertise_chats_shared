9c8defa332b282a323e3f26a9090a4fe9d117937
BrenDev0 <lahey1991@gmail.com>
2025-11-27 09:20:24 -0600

    export error

--- expertise_chats/broker/__init__.py
+++ expertise_chats/broker/__init__.py
@@ -4,15 +4,15 @@ Message broker package for expertise_chats.
 """
 
 from expertise_chats.broker.infrastructure.rabbitMq.connection import RabbitMqConnection as BrokerConnection
-from expertise_chats.broker.infrastructure.rabbitMq.consumer import RabbitMqConnection as Consumer
+from expertise_chats.broker.infrastructure.rabbitMq.consumer import RabbitMqConsumer as Consumer
 from expertise_chats.broker.infrastructure.rabbitMq.producer import RabbitMqProducer as Producer
-from expertise_chats.broker.domain.event_handler import EventHandler, AsyncEventHandler
+from expertise_chats.broker.domain.event_handler import EventHandler as EventHandlerBase, AsyncEventHandler as AsyncEventHandlerBase
 
 # Re-export the main classes users need
 __all__ = [
    "BrokerConnection",
    "Consumer",
    "Producer",
-   "EventHandler",
-   "AsyncEventHandler"
+   "EventHandlerBase",
+   "AsyncEventHandlerBase"
 ]
