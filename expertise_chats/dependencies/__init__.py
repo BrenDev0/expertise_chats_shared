a5de8fc17d662a18da8301aa0d119ceba784506f
BrenDev0 <lahey1991@gmail.com>
2025-12-04 10:49:44 -0600

    dependencies

---
+++ expertise_chats/dependencies/__init__.py
@@ -0,0 +1,10 @@
+"""
+Dependency package for expertise_chats.
+"""
+
+from expertise_chats.dependencies.container import Container
+
+
+__all__ = [
+   "Container"
+]
---
+++ expertise_chats/exceptions/__init__.py
@@ -0,0 +1,9 @@
+"""
+Exceptions package for expertise_chats.
+"""
+
+from expertise_chats.exceptions.dependencies import DependencyNotRegistered
+
+__all__ = [
+   "DependencyNotRegistered"
+]
