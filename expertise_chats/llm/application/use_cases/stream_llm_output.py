2e06518e955f69ad6bc1823491692d197744c824
BrenDev0 <lahey1991@gmail.com>
2025-12-05 12:47:47 -0600

    removed producer

--- expertise_chats/llm/application/use_cases/stream_llm_output.py
+++ expertise_chats/llm/application/use_cases/stream_llm_output.py
@@ -2,21 +2,17 @@ from expertise_chats.llm.domain.services.llm_service import LlmService
 from expertise_chats.llm.application.use_cases.handle_chunk import HandleChunk
 from expertise_chats.llm.application.use_cases.end_audio_stream import EndAudioStream
 from expertise_chats.broker.domain.interaction_event import InteractionEvent
-from expertise_chats.broker.domain.producer import Producer
-
 
 class StreamLlmOutput:
     def __init__(
         self,
         llm_service: LlmService,
         chunk_hander: HandleChunk,
-        end_audio_stream: EndAudioStream,
-        producer: Producer
+        end_audio_stream: EndAudioStream
     ):
         self.__llm_service = llm_service
         self.__chunk_handler = chunk_hander
         self.__end_audio_stream = end_audio_stream
-        self.__producer = producer
 
     async def execute(
         self,
@@ -44,12 +40,5 @@ class StreamLlmOutput:
                 sentence=sentence,
                 event=event
             )
-        
-        self.__producer.publish(
-            routing_key="messages.outgoing.send",
-            event_message={
-                "llm_response": "".join(chunks)
-            }
-        )
 
         return "".join(chunks)
