bb2c93fd42bc2e504078b46751ef51b4c05ebfb1
BrenDev0 <lahey1991@gmail.com>
2025-12-05 11:21:10 -0600

    vector service  to repository

--- expertise_chats/llm/__init__.py
+++ expertise_chats/llm/__init__.py
@@ -5,7 +5,7 @@ LLM package for expertise_chats.
 
 from expertise_chats.llm.infrastructure.langchain.llm_service import LangchainLlmService as LlmService
 from expertise_chats.llm.infrastructure.openai.embedding_service import OpenAIEmbeddingService as EmbeddingService
-from expertise_chats.llm.infrastructure.qdrant.vector_repository import QdrantVectorRepository as VectorService
+from expertise_chats.llm.infrastructure.qdrant.vector_repository import QdrantVectorRepository as VectorRepository
 from expertise_chats.llm.domain.services.workflow_service import WorkflowService as WorkflowServiceAbsract
 from expertise_chats.llm.application.use_cases.handle_chunk import HandleChunk
 from expertise_chats.llm.application.use_cases.end_audio_stream import EndAudioStream
@@ -19,7 +19,7 @@ from expertise_chats.llm.application.use_cases.search_for_context import SearchF
 __all__ = [
    "LlmService",
    "EmbeddingService",
-   "VectorService",
+   "VectorRepository",
    "WorkflowServiceAbsract",
    "HandleChunk",
    "EndAudioStream",
