4acd4eb76026e37fae2e5ce7e8d661c00732e561
BrenDev0 <lahey1991@gmail.com>
2025-12-05 10:33:39 -0600

    end  audio stream use case

--- expertise_chats/llm/__init__.py
+++ expertise_chats/llm/__init__.py
@@ -3,7 +3,9 @@ LLM package for expertise_chats.
 """
 
 from expertise_chats.llm.application.use_cases.handle_chunk import HandleChunk
+from expertise_chats.llm.application.use_cases.end_audio_stream import EndAudioStream
 
 __all__ = [
-   "HandleChunk"
+   "HandleChunk",
+   "EndAudioStream"
 ]
---
+++ expertise_chats/llm/application/use_cases/end_audio_stream.py
@@ -0,0 +1,37 @@
+from  expertise_chats.broker.domain.interaction_event import InteractionEvent
+from expertise_chats.schemas.ws import WsPayload
+from expertise_chats.broker.infrastructure.rabbitMq.producer import RabbitMqProducer as Producer
+
+class EndAudioStream:
+    def __init__(
+        self,
+        producer: Producer
+    ):
+        self.__producer = producer
+
+    def execute(
+        self,
+        sentence: str,
+        event: InteractionEvent
+    ):
+        ws_payload = WsPayload(
+            type="AUIDO",
+            data=sentence.strip()
+        )
+
+        event.event_data =ws_payload.model_dump()
+
+        self.__producer.publish(
+            routing_key=f"streaming.audio.outbound.send",
+            event_message=event
+        )
+
+        ws_payload.type = "END"
+        ws_payload.data = "END STREAM"
+
+        event.event_data = ws_payload.model_dump()
+
+        self.__producer.publish(
+            routing_key=f"streaming.general.outbound.send",
+            event_message=event
+        )
